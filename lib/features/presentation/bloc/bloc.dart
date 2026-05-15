dbc7ccc2b2fb3393c4c2948492638a65440ba41f
Rafael-VH <rafaelvillahinojosa@gmail.com>
2026-05-15 11:17:42 -0400

    feat: implement GenreBloc with LoadGenres event and Loading/Loaded/Error states
    - Add GenreBloc, GenreEvent, and GenreState
    - Export GenreBloc from bloc.dart barrel file

--- lib/features/presentation/bloc/bloc.dart
+++ lib/features/presentation/bloc/bloc.dart
@@ -1,9 +1,12 @@
 //  BLOC
 export 'package:noveles/features/presentation/bloc/book_bloc.dart';
+export 'package:noveles/features/presentation/bloc/genre_bloc.dart';
 //  EVENT
 export 'package:noveles/features/presentation/bloc/book_event.dart';
+export 'package:noveles/features/presentation/bloc/genre_event.dart';
 //  STATE
 export 'package:noveles/features/presentation/bloc/book_state.dart';
+export 'package:noveles/features/presentation/bloc/genre_state.dart';
 export 'package:noveles/features/presentation/bloc/theme_bloc.dart';
 export 'package:noveles/features/presentation/bloc/theme_event.dart';
 export 'package:noveles/features/presentation/bloc/theme_state.dart';
---
+++ lib/features/presentation/bloc/genre_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:noveles/features/domain/use_cases/use_cases.dart';
+import 'package:noveles/features/presentation/bloc/genre_event.dart';
+import 'package:noveles/features/presentation/bloc/genre_state.dart';
+
+class GenreBloc extends Bloc<GenreEvent, GenreState> {
+  final GetGenre getGenre;
+
+  GenreBloc(this.getGenre) : super(GenreInitial()) {
+    on<LoadGenres>(_onLoadGenres);
+  }
+
+  Future<void> _onLoadGenres(
+    LoadGenres event,
+    Emitter<GenreState> emit,
+  ) async {
+    emit(GenreLoading());
+    try {
+      final genres = await getGenre();
+      emit(GenreLoaded(genres));
+    } catch (e) {
+      emit(GenreError(e.toString()));
+    }
+  }
+}
